0c7d7b81d8888d156e4f8cea22f9ebc358691065
Jeff Mitchell <jeffreytm@gmail.com>
2024-11-12 13:46:27 -0500

    update to  js module

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import { fetchWordData } from "./api.js";
+
+import { saveFavorite, displayFavorites } from "./favorites.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  setupSearch(); // Set up the search functionality
+  displayFavorites(); // Display saved favorites when the page loads
+});
